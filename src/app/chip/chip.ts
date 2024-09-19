e92c6c99b8610e30874f8735a0b7c19565edb126
Vinicius Afonso Raimundo Ferreira <72279560+viniciusferreira87@users.noreply.github.com>
2024-09-19 11:13:26 -0300

    Multiselect

--- src/app/chip/chip.ts
+++ src/app/chip/chip.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,6 +7,11 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
+import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray} from '@angular/cdk/drag-drop';
+
+export interface Vegetable {
+  name: string;
+}
 
 @Component({
   selector: 'component-card',
@@ -20,7 +25,10 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatTabsModule,
     MatListModule,
     MatCardModule,
+    CdkDropList, 
+    CdkDrag
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <div class="component-card">
       <ng-content></ng-content>
@@ -30,4 +38,20 @@ import { MatTabsModule } from '@angular/material/tabs';
     templateUrl: 'chip.html',
   styleUrls: ['../app.component.scss'],
 })
-export class Chip {}
+
+export class Chip {
+  readonly vegetables = signal<Vegetable[]>([
+    {name: 'apple'},
+    {name: 'banana'},
+    {name: 'strawberry'},
+    {name: 'orange'},
+    {name: 'kiwi'},
+    {name: 'cherry'},
+  ]);
+
+  drop(event: CdkDragDrop<Vegetable[]>) {
+    this.vegetables.update(vegetables => {
+      moveItemInArray(vegetables, event.previousIndex, event.currentIndex);
+      return [...vegetables];
+    });
+  }}
